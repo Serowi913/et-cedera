ff7336a9d18315616e85fcf57b50486cf8482a7d
Serowi913 <73836434+Serowi913@users.noreply.github.com>
2022-10-21 20:34:30 -0400

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def parse_for_definition(word):
+    listx = str(requests.get(
+        f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+        .content).split(
+            '"definition":"')
+
+    b = 0
+    for i in listx:
+        x = i.split('","')
+        listx[b] = x[0]
+        b += 1
+    listx.pop(0)
+
+    return listx
