fbe9b4e2b4694b0607491ed925d9e7cde0293c28
Serowi913 <73836434+Serowi913@users.noreply.github.com>
2022-11-09 19:00:29 -0500

    Update color_generator_w_names.py

--- python/color_generator_w_names.py
+++ python/color_generator_w_names.py
@@ -1,12 +1,13 @@
 from requests import get as g
-from PIL.ImageColor import getcolor as c
+from PIL.ImageColor import getcolor as c # function to convert hex to RGB
 
 class Color:
     def __init__(self):
-        self.color = str(g("https://colornames.org/random/json/").content).split('"')
-        self.name = self.color[7]
-        self.hex_code = self.color[3]
+        clr_request = str(g("https://colornames.org/random/json/").content).split('"') # request JSON, makes list
+        self.color = [clr_request[7], clr_request[3]] # make a list of only the name and hex value
+        self.name = self.color[0]
+        self.hex_code = self.color[1]
         self.rgb = self.con_rgb()
     
     def con_rgb(self):
-        return c("#" + self.hex_code, "RGB")
+        return c("#" + self.hex_code, "RGB") # converts hex to RGB
